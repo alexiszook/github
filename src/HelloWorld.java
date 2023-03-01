c526e15d8ed7569929caf0911696101328badd7b
COLLINSNL21 <collinsnl21@gcc.edu>
2023-03-01 10:40:43 -0500

    Asked user for their name and returned "hello (name)"

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        Scanner scr = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = scr.next();
+        System.out.println("Hello " + name);
     }
 }
